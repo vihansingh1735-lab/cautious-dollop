b0580f334cf9c034b18e2f3e250869fc3db5f073
istealyourhome <vihansingh1735@gmail.com>
2026-05-24 22:19:45 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Thunder Kartells"
+};
+
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
