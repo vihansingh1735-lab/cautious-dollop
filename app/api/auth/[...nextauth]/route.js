353022b98528177c14d8e43ff43b074b727baa46
istealyourhome <vihansingh1735@gmail.com>
2026-05-25 20:50:23 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,25 +2,13 @@ import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 const handler = NextAuth({
-
   providers: [
-
     DiscordProvider({
-
       clientId: process.env.DISCORD_CLIENT_ID,
-
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
-
-      authorization: {
-        params: {
-          scope: "identify guilds guilds.join"
-        }
-      }
-
+      clientSecret: process.env.DISCORD_CLIENT_SECRET
     })
-
-  ]
-
+  ],
+  secret: process.env.NEXTAUTH_SECRET
 });
 
 export { handler as GET, handler as POST };
