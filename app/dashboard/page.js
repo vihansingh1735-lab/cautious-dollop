ec92521fd9eaaeb6d48c175678aaa3e12d5d1d5d
istealyourhome <vihansingh1735@gmail.com>
2026-05-25 18:14:59 +0530

    Create page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,15 @@
+export default function Dashboard() {
+
+  return (
+
+    <main className="bg-black text-white min-h-screen p-10">
+
+      <h1 className="text-5xl font-bold">
+        Dashboard
+      </h1>
+
+    </main>
+
+  );
+
+}
