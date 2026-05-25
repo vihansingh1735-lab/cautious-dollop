a94a68012800fe222219384cbd305c5a5f2fc56c
istealyourhome <vihansingh1735@gmail.com>
2026-05-25 20:29:29 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -34,11 +34,11 @@ export default function Home() {
 
   useEffect(() => {
 
-    fetch("http://51.83.6.7:20253/api/gang", {
+    fetch("/api/gang", {
   headers: {
     "x-api-key": "dp_live_JbLRZzyMCZ0HAS9t7WpRmomM"
   }
-})
+}) {
   .then(res => res.json())
   .then(data => setMembers(data))
   .catch(console.error);
